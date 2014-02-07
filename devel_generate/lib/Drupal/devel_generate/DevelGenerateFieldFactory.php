2044870c10b41d24c56f542418925c6bf70fddbb
Enxebre <you@yourdomain.example.com>
2014-02-07 16:36:23 +0000

    allowing overide for all field classes

--- devel_generate/lib/Drupal/devel_generate/DevelGenerateFieldFactory.php
+++ devel_generate/lib/Drupal/devel_generate/DevelGenerateFieldFactory.php
@@ -6,10 +6,13 @@ class DevelGenerateFieldFactory {
 
   public function createInstance($fieldType) {
     $fieldType = ucfirst($fieldType);
-    $class = "Drupal\devel_generate\DevelGenerateField$fieldType";
+    $class = "Drupal\devel_generate\DevelGenerateField${fieldType}Custom";
 
     if (!class_exists($class)) {
-      throw new DevelGenerateException(sprintf('The field type (%s) did not specify an instance class.', $fieldType));
+      $class = "Drupal\devel_generate\DevelGenerateField${fieldType}";
+      if (!class_exists($class)) {
+        throw new DevelGenerateException(sprintf('The field type (%s) did not specify an instance class.', $fieldType));
+      }
     }
 
     return new $class();
